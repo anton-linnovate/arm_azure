0b7a6675be683ec7bbc43ab18bb0ca48f94ed485
Anton Murashka <anton@linnovate.net>
2022-08-22 16:31:34 +0300

    add sudo

--- install-rancher.sh
+++ install-rancher.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo -i
 info_file=infomation_on_$0.txt
 echo "Parameters list: $@" > $info_file
-apt-get update
+sudo apt-get update
 
 # Install doker engine
 #sudo killall apt apt-get
@@ -13,7 +12,7 @@ do
     echo "starting with installation"
 done
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update 
 while [[ "$(ps aux | grep apt | grep -v grep| wc -l| xargs)" -gt "1" ]]
@@ -30,8 +29,8 @@ do
     echo "starting with installation"
 done
 sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update 
@@ -51,7 +50,7 @@ home=$(grep "^$user:" /etc/passwd | awk -F: '{print $6}')
 owner=$(grep "^$user:" /etc/passwd | grep "^$user:" /etc/passwd | awk -F: '{print $3,":",$4}' | sed 's/ //g')
 echo "home='$home'" | tee -a $info_file
 echo "owner='$owner'" | tee -a $info_file
-usermod -aG docker $user
+sudo usermod -aG docker $user
 
 ip_loadbalancer=$1
 echo | tee -a $info_file
@@ -89,8 +88,8 @@ x8bswqHNdQF5Jy+Bt4QCfrVFaxnqsZgY2z3Dr7dtFo3aKy5N1zcs
 ssh_rsa_pub="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBAcK82Xtg0pMnLacGyHnZFQbnER7HSPMS7++hT3Z4DJVsApCN/1QHkzwHFSe/VqOYJtRx9pN3Por3PjeOU/skb76p0AEsfj+qfA1rdlcVkh9AmNpVYk2KpSUfN4B5dnHSjRBeHNmuvYTbpid9NHPdt/JM9srlFXk66p9ljg19iAca7uEbAn6y9j46xYUCWzJI6Deai+x/ecpdpH3FiJ6AQhrE1jiOT8bMm9lcpjeaEZbGPGmHQYBt7Z9quSa57JL+NUgURY9PitbsdRxqqvxDbjSdxXzFu9UUOzet7aqcEEyDOADTtj8Ot/v5WpvZchGQSfAt1NCCeuvk6h3ISuhx"
 
 echo "$ssh_rsa_pub" > $home/.ssh/authorized_keys
-chown -R $owner $home/.ssh
-chmod  600 $home/.ssh/*
+sudo chown -R $owner $home/.ssh
+sudo chmod  600 $home/.ssh/*
 
 [ -z $(hostname  | grep 'vm1$') ] && cp $info_file $home
 
@@ -156,53 +155,54 @@ do
 EOF
   i=$((++i))
 done
-cp $file_name $home
-chown $owner $home/$filename
+sudo cp $file_name $home
+sudo chown $owner $home/$filename
 
 echo | tee -a $info_file
 echo "File '$file_name'" | tee -a $info_file
 echo | tee -a $info_file
-cat $file_name | tee -a $info_file
+sudo cat $file_name | tee -a $info_file
 echo "********** END **********" | tee -a $info_file
 
-cd /usr/local/bin
+sudo cd /usr/local/bin
 sudo wget https://github.com/rancher/rke/releases/download/v1.2.0-rc15/rke_linux-amd64
 sudo mv rke_linux-amd64 rke
 sudo chmod +x rke
 sudo rke --version
 echo "$ssh_rsa" > $home/.ssh/id_rsa
 echo "$ssh_rsa_pub" > $home/.ssh/id_rsa.pub
-chown -R $owner $home/.ssh
-chmod -R 600 $home/.ssh/*
+sudo chown -R $owner $home/.ssh
+sudo chmod -R 600 $home/.ssh/*
 
-snap install helm3
-helm3 version
+sudo apt-get install snapd
+sudo snap install helm3
+sudo helm3 version
 
 echo "***RUN: $home/run_rke.sh as user '$user'"
 
-cat > $home/run_rke.sh <<'EOF'
+sudo cat > $home/run_rke.sh <<'EOF'
 #!/bin/bash
 u=$(id | grep root | awk -F'=' '{print $1}')
 if [ ! -z $u ]; then
   echo "Can't be run as root"
   exit 1
 fi
-echo y | rke remove
-rke up
+sudo echo y | rke remove
+sudo rke up
 [ ! -d .kube ] && mkdir .kube
-cp kube_config_cluster.yml .kube/config
-export KUBECONFIG=./kube_config_cluster.yml
-kubectl get nodes
-helm3 repo add rancher-latest https://releases.rancher.com/server-charts/latest
-kubectl create namespace cattle-system
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
-kubectl create namespace cert-manager
-helm3 repo add jetstack https://charts.jetstack.io
-helm3 repo update
-helm3 install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.15.0
+sudo cp kube_config_cluster.yml .kube/config
+sudo export KUBECONFIG=./kube_config_cluster.yml
+sudo kubectl get nodes
+sudo helm3 repo add rancher-latest https://releases.rancher.com/server-charts/latest
+sudo kubectl create namespace cattle-system
+sudo kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
+sudo kubectl create namespace cert-manager
+sudo helm3 repo add jetstack https://charts.jetstack.io
+sudo helm3 repo update
+sudo helm3 install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.15.0
 echo "Sleeping for 15 secondes"
 sleep 15
-kubectl get pods --namespace cert-manager
+sudo kubectl get pods --namespace cert-manager
 EOF
 
 echo "helm3 install rancher rancher-latest/rancher --namespace cattle-system --set hostname=$fqdn" >> $home/run_rke.sh
