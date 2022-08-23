7cf07c66f1fc2b6e6d339163bb2ab2417adc8909
Anton Murashka <anton@linnovate.net>
2022-08-23 16:34:41 +0300

    new fix

--- install-rancher.sh
+++ install-rancher.sh
@@ -12,6 +12,7 @@ do
     echo "starting with installation"
 done
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y dialog apt-utils 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update 
@@ -209,6 +210,6 @@ echo "sudo helm3 install rancher rancher-latest/rancher --namespace cattle-syste
 echo "echo hostname = '$fqdn'" >> $home/run_rke.sh 
 echo "echo '*************  End of story  *************'" >> $home/run_rke.sh 
 
-sudo cd $home
+cd $home
 sudo chmod +x run_rke.sh
 sudo ./run_rke.sh
